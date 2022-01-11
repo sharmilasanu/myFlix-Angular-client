441b1fb3aabba79598d0ba4c1929fed8f2a1ad32
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-01-11 16:51:13 +0100

    Update user-login-form.component.ts

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input} from '@angular/core';
 import { UserRegistrationService } from '../fetch-api-data.service';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
@@ -12,8 +13,10 @@ export class UserLoginFormComponent implements OnInit {
   @Input() loginCredentials = { UserName: '', Password: '' };
   constructor(
     public fetchApiData: UserRegistrationService,
+    private router  : Router,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
     public snackBar: MatSnackBar) { }
+    
 
   ngOnInit(): void {
   }
@@ -21,14 +24,14 @@ export class UserLoginFormComponent implements OnInit {
     this.fetchApiData.userLogin(this.loginCredentials).subscribe((result) => {
   // Logic for a successful user registration goes here! (To be implemented)
 
-  
+      
      this.dialogRef.close(); // This will close the modal on success!
      localStorage.setItem('token', result.token);
      localStorage.setItem('user', JSON.stringify(result.user));
      console.log(result)
      this.snackBar.open(result, 'OK', {
         duration: 2000
-     });
+     });this.router.navigate(['movies']);
     }, (result) => {
       console.log(result)
       this.snackBar.open(result, 'OK', {
