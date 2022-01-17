4f17712103943308ede2b18c0175d238700cd533
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-01-17 06:01:19 +0100

    Create synopsis.component.ts

---
+++ src/app/synopsis/synopsis.component.ts
@@ -0,0 +1,17 @@
+import { Component,Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-synopsis',
+  templateUrl: './synopsis.component.html',
+  styleUrls: ['./synopsis.component.scss']
+})
+export class SynopsisComponent implements OnInit {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { title: string, description: string }) { }
+
+
+  ngOnInit(): void {
+  }
+
+}
