5881ac1205977dc940434d2d65c9b6334d5b42a4
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-01-19 05:10:01 +0100

    Typedoc implementation

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-// You'll use this import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
-
-// This import brings in the API calls we created in 6.2
 import { UserRegistrationService } from '../fetch-api-data.service';
-
-// This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-user-registration-form',
@@ -14,6 +9,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class UserRegistrationFormComponent implements OnInit {
 
+ //Binds input values to userData object
+   
   @Input() userData = { UserName: '', Password: '', Email: '', Birthday: '' };
 
 constructor(
@@ -25,10 +22,12 @@ ngOnInit(): void {
 }
 
 // This is the function responsible for sending the form inputs to the backend
+  /**
+   * Function for sending the form inputs to the backend to create a new user
+   * @returns alert indicating a successful registration or an error
+   */
 registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
-  // Logic for a successful user registration goes here! (To be implemented)
-
   
      this.dialogRef.close(); // This will close the modal on success!
      console.log(result)
