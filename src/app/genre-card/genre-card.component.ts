6d2291ad20d95c2a82cf8b30b3f76c7d1c1c2d2b
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-01-19 05:09:37 +0100

    Typedoc implementation

--- src/app/genre-card/genre-card.component.ts
+++ src/app/genre-card/genre-card.component.ts
@@ -7,8 +7,15 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./genre-card.component.scss']
 })
 export class GenreCardComponent implements OnInit {
-
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { name: string, description: string }) { }
+/**
+   * Called when creating an instance of the class
+   * @param data {object}
+   */
+  constructor(
+    //Injection token that can be used to access the data that was passed in to a dialog.
+    @Inject(MAT_DIALOG_DATA) 
+    public data: { name: string, description: string }
+    ) {}
 
   ngOnInit(): void {
   }
